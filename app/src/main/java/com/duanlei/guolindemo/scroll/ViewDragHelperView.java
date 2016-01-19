c71fce3a5799b00bddc6beae483aca662f44dc9c
duanlei <455483293@qq.com>
2016-01-19 11:00:32 +0800

    调用另一个app的页面

--- app/src/main/java/com/duanlei/guolindemo/scroll/ViewDragHelperView.java
+++ app/src/main/java/com/duanlei/guolindemo/scroll/ViewDragHelperView.java
@@ -6,13 +6,13 @@ import android.support.v4.widget.ViewDragHelper;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 /**
  * Author: duanlei
  * Date: 2016-01-14
  */
-public class ViewDragHelperView extends ViewGroup {
+public class ViewDragHelperView extends FrameLayout {
 
     public ViewDragHelperView(Context context) {
         this(context, null);
@@ -31,14 +31,23 @@ public class ViewDragHelperView extends ViewGroup {
 
     private View mMainView;
     private View mMenuView;
+    private int mWidth;
 
     private void init() {
         mViewDragHelper = ViewDragHelper.create(this, callback);
     }
 
     @Override
-    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        mMenuView = getChildAt(0);
+        mMainView = getChildAt(1);
+    }
 
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mWidth = mMenuView.getMeasuredWidth();
     }
 
     @Override
@@ -64,13 +73,39 @@ public class ViewDragHelperView extends ViewGroup {
     private ViewDragHelper.Callback callback = new ViewDragHelper.Callback() {
         @Override
         public boolean tryCaptureView(View child, int pointerId) {
-
             //如果当前触摸的child是mMainView是开始检测
-
             return mMainView == child;
         }
+
+        //处理垂直滑动
+        @Override
+        public int clampViewPositionVertical(View child, int top, int dy) {
+            return 0;
+        }
+
+        //处理水平滑动
+        @Override
+        public int clampViewPositionHorizontal(View child, int left, int dx) {
+            return left;
+        }
+
+        //拖动结束后调用
+
+        @Override
+        public void onViewReleased(View releasedChild, float xvel, float yvel) {
+            super.onViewReleased(releasedChild, xvel, yvel);
+            //手指抬起后缓慢移动到指定位置
+            if (mMainView.getLeft() < 500) {
+                //关闭菜单
+                //相当于Scroller的startScroll方法
+                mViewDragHelper.smoothSlideViewTo(mMainView, 0, 0);
+                ViewCompat.postInvalidateOnAnimation(ViewDragHelperView.this);
+            } else {
+                //打开菜单
+                mViewDragHelper.smoothSlideViewTo(mMainView, 300, 0);
+                ViewCompat.postInvalidateOnAnimation(ViewDragHelperView.this);
+            }
+        }
     };
-
-
 
 }
